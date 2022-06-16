890efcb4de77939f9d9cbd5d480b2dccea131126
Grzegorz <grzegorzgulba@gmail.com>
2022-06-16 13:21:27 +0200

    Initial Commit

--- src/main/java/PrzykladowaKlasa.java
+++ src/main/java/PrzykladowaKlasa.java
@@ -3,5 +3,6 @@ public class PrzykladowaKlasa {
     public static void main(String[] args) {
         System.out.println("Przykladowa klasa");
         System.out.println("1234");
+        System.out.println("1234");
     }
 }
